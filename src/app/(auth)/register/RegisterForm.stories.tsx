c0cab4a603c5115b16cc4e889119b1f89acc603c
sugawaramasaya <masaya.sugawara@gmail.com>
2025-06-14 14:39:47 +0900

    Implement AuthForm with isLogin toggle and fix related issues

--- src/app/(auth)/register/RegisterForm.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import RegisterPage from "./page";
-
-export default {
-  title: "Auth/RegisterPage",
-  component: RegisterPage,
-};
-
-export const Default = () => <RegisterPage />;
---
+++ src/app/(auth)/register/email/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from "@/components/AuthForm";
+
+export default function RegisterFormPage() {
+  return <AuthForm isLogin={false} />;
+}
