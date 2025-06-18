4d2f61afca27d461657b1564537a1f4c63c479a7
sugawaramasaya <masaya.sugawara@gmail.com>
2025-06-18 11:54:34 +0900

    ロゴとnidomyがいつの間にか削除されていたので修正、その他レイアウト崩れをすべて修正

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,20 +1,45 @@
 "use client";
 import React from "react";
+import Image from "next/image";
 import Button from "@/components/Button";
 import FixedBottomContainer from "@/components/FixedBottomContainer";
+import nidomy from "@/assets/nidomy/nidomy.png";
+import LogoSvg from "@/assets/logo.svg";
 
 export default function SignupPage() {
   return (
     <div className="h-screen flex flex-col justify-between">
       <div className="w-full max-w-[480px] mx-auto flex flex-col items-center h-screen">
+        {/* ロゴ・nidomy画像を縦flexで並べる。下部ブロック分スペースを空ける */}
+        <div className="flex flex-col w-full items-center flex-1 min-h-0">
+          {/* ロゴ */}
+          <div className="w-full flex justify-center items-center pt-[24px] px-[24px] z-20">
+            <div className="w-full aspect-[100/18]">
+              <LogoSvg className="w-full h-full block" />
+            </div>
+          </div>
+          {/* nidomy画像を中央配置 */}
+          <div className="flex-1 flex items-center justify-center w-full">
+            <div className="aspect-square flex justify-center items-center w-full">
+              <Image
+                src={nidomy.src}
+                alt="nidomy graphic"
+                width={256}
+                height={256}
+                priority
+                className="aspect-square"
+              />
+            </div>
+          </div>
+        </div>
         {/* メインコンテンツ */}
-        <div className="flex flex-col w-full p-[24px] gap-[24px] flex-1">
-          {/* Googleで登録ボタン */}
-          <Button fullWidth variant="primary">
-            Googleで登録
-          </Button>
+        <div className="flex flex-col w-full px-[16px] pb-[40px] gap-[24px]">
           {/* メールアドレスで登録ボタンと注釈 */}
           <div className="flex flex-col gap-[12px]">
+            {/* Googleで登録ボタン */}
+            <Button fullWidth variant="primary">
+              Googleで登録
+            </Button>
             <div
               className="text-center"
               style={{
@@ -39,7 +64,7 @@ export default function SignupPage() {
                   color: "var(--on-surface-variant)",
                 }}
               >
-                <span>続行することで、</span>Add commentMore actions
+                <span>続行することで、</span>
                 <a href="#" className="text-nidomi-blue-70 underline">
                   利用規約
                 </a>
@@ -55,13 +80,10 @@ export default function SignupPage() {
               </div>
             </div>
           </div>
-        </div>
-        {/* 下部固定のログインボタン */}
-        <FixedBottomContainer>
           <Button fullWidth variant="secondary">
             ログイン
           </Button>
-        </FixedBottomContainer>
+        </div>
       </div>
     </div>
   );
