10eb17077046298e02722f9bbe9a303df8674efd
sugawaramasaya <masaya.sugawara@gmail.com>
2025-06-18 12:40:11 +0900

    不要なディレクトリ、ファイルを削除

--- src/app/(auth)/register/verify/page.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/(auth)/register/verify/page.tsx
-export default function VerifyPage() {
-  return (
-    <div className="flex items-center justify-center min-h-screen text-xl">
-      仮の認証ページです（Verify）
-    </div>
-  );
-}
